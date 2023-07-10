2d403861b3d355aebdb537c97f8083fcf81ba930
Oğuzhan Nejat Karabaş <karabasnejat@hotmail.com>
2023-07-10 06:14:49 +0300

    Proje dosyası ekle.

---
+++ CrudApp/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudApp.Models
+{
+	public class Employee
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[DisplayName("First Name:")]
+		[Required]
+		public string FirstName { get; set; }
+
+		[DisplayName("Last Name:")]
+		[Required]
+		public string LastName { get; set; }
+
+		[DisplayName("Date of Birth:")]
+		[Required]
+		public DateTime DateOfBirth { get; set; }
+		
+		[DisplayName("E-Mail:")]
+		[Required]
+
+		public string Email { get; set; }
+		[Required]
+		public double Salary { get; set; }
+		
+		[NotMapped]
+		public string FullName 
+		{
+			get {return FirstName + " " + LastName; }
+		}
+
+		
+
+
+	}
+}
